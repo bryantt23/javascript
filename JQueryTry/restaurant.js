2d916daa0697cb96e8d83b90290b487bde17f377
bryantt23 <bryantt23@gmail.com>
2015-04-15 00:15:20 -0700

    adds to page with jquery append

---
+++ JQueryTry/restaurant.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("div.container").append("<header></header>");
+        $("div.container").append("<h1> great veggie burgers </h1>");
+
+            $("div.container").append('<img src="veggie.jpg" height="64px" width="64px">');
+
+                        $("div.container").append("<p> tasty </p>");
+
+    });
